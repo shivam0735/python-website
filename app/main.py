57bf0b8a3e0f659c4d59fc995a2e0520b91b7de3
Shivam Jhunjhunu Wala <shivamj2005@gmail.com>
2020-08-15 17:56:47 +0530

    Add greeting app and frontend

--- app/main.py
+++ app/main.py
@@ -1,7 +1,22 @@
-import flask
+from flask import Flask, jsonify
+from flask_cors import CORS
 
+app = Flask(__name__)
+CORS(app)
+
+global_history = []
+
+@app.route('/')
 def hello():
     return "Hello World!"
 
+@app.route('/greeting/<name>')
 def greeting(name):
+    global_history.append(name)
     return "Hello! " + name
+
+@app.route('/history')
+def get_global_history():
+    return jsonify(global_history)
+
+app.run(debug=True, host="0.0.0.0")
--- website/index.js
+++ website/index.js
@@ -1,3 +1,51 @@
-$(document).ready(function() {
-    
+$(document).ready(function(){
+    console.log("Hello World!");
+    $("#my-button").click(get_content);
+    $.get("http://192.168.29.218:5000/history", update_global_history)
 });
+
+var local_history = [];
+
+function change_content(data) {
+    $('#my-content').text(data);
+    update_local_history();
+    $.get("http://192.168.29.218:5000/history", update_global_history)
+}
+
+function update_local_history() {
+    // <ul>
+    // <li>..</li>
+    // <li>..</li>
+    // <li>..</li>
+    // </ul>
+    var my_list = "<ul>";
+    local_history.forEach(function(elem){
+       my_list += "<li>" + elem + "</li>"
+    });
+    my_list += "</ul>";
+    $("#local-history-box").html(my_list);
+}
+
+function update_global_history(global_history) {
+    // <ul>
+    // <li>..</li>
+    // <li>..</li>
+    // <li>..</li>
+    // </ul>
+    var my_list = "<ul>";
+    global_history.forEach(function(elem){
+       my_list += "<li>" + elem + "</li>"
+    });
+    my_list += "</ul>";
+    $("#global-history-box").html(my_list);
+}
+
+function get_content() {
+    var requested_name = $("#my-name").val();
+    if(requested_name == "") {
+        alert("Enter something");
+        return;
+    }
+    local_history.push(requested_name);
+    $.get("http://192.168.29.218:5000/greeting/" + requested_name, change_content)
+}
